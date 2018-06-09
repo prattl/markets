61c725d960b37f7420b64343c684a5a4e59b9b17
Lenny Pratt <lenny@prattdev.net>
2018-06-09 11:34:49 -0400

    Split tabs into their own components and track active tab in model

--- src/App.elm
+++ src/App.elm
@@ -5,25 +5,57 @@ import Html.Attributes
     exposing
         ( class
         , placeholder
+        , style
         , type_
         )
+import Html.Events exposing (onClick)
 import Bulma.Modifiers exposing (..)
+import Bulma.Components exposing (..)
 import Bulma.Elements exposing (..)
 import Bulma.Form exposing (..)
 import Bulma.Layout exposing (..)
 
 
-type alias Model =
-    { zip : String }
-
-
-main : Program Never Model msg
+main : Program Never Model Msg
 main =
     Html.beginnerProgram
-        { model = { zip = "" }
+        { model = { zip = "", activeTab = 0 }
         , view = view
-        , update = \msg -> \model -> model
+        , update = update
         }
+
+
+
+-- MODEL
+
+
+type alias TabIndex =
+    Int
+
+
+type alias Model =
+    { zip : String
+    , activeTab : TabIndex
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ChangeTab TabIndex
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ChangeTab newTab ->
+            { model | activeTab = newTab }
+
+
+
+-- VIEW
 
 
 marketsHero : Html msg
@@ -59,12 +91,52 @@ searchForm =
         [ searchInput ]
 
 
-appContainer : Html msg
-appContainer =
+tabMods : TabsModifiers
+tabMods =
+    TabsModifiers Minimal Left Large
+
+
+tabOptions : List (Html msg)
+tabOptions =
+    [ Html.i [ class "fas fa-table" ] []
+    , Html.i [ class "fa fa-map-marker-alt" ] []
+    ]
+
+
+renderTab : TabIndex -> Int -> Html msg -> Html msg
+renderTab activeTab index child =
+    tab
+        (activeTab == index)
+        []
+        []
+        [ child ]
+
+
+renderTabs : Model -> List (Tab msg)
+renderTabs model =
+    List.indexedMap (renderTab model.activeTab) tabOptions
+
+
+resultsTabs : Model -> Html msg
+resultsTabs model =
+    tabs
+        tabMods
+        []
+        []
+    <|
+        renderTabs model
+
+
+appContainer : Model -> Html msg
+appContainer model =
     section NotSpaced
         []
         [ container []
-            [ searchForm ]
+            [ searchForm
+            , Html.div
+                [ style [ ( "margin-top", "3rem" ) ] ]
+                [ resultsTabs model ]
+            ]
         ]
 
 
@@ -72,5 +144,5 @@ view : Model -> Html msg
 view model =
     Html.main_ []
         [ marketsHero
-        , appContainer
+        , appContainer model
         ]
