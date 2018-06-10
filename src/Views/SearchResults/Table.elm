e7f4338b66249220183012075f44d47b61b1beae
Lenny Pratt <lenny@prattdev.net>
2018-06-10 18:45:33 -0400

    Display search results in table

--- src/Views/SearchResults/Table.elm
+++ src/Views/SearchResults/Table.elm
@@ -1,9 +1,45 @@
 module Views.SearchResults.Table exposing (searchResultsTable)
 
 import App.Types exposing (..)
+import Bulma.Elements exposing (..)
 import Html exposing (Html)
+
+
+searchResultsRow : FarmersMarket -> TableRow Msg
+searchResultsRow farmersMarket =
+    tableRow
+        False
+        []
+        [ tableCell [] [ Html.text farmersMarket.id ]
+        , tableCell [] [ Html.text farmersMarket.marketname ]
+        ]
+
+
+searchResultsBody : Maybe (List FarmersMarket) -> TablePartition Msg
+searchResultsBody results =
+    tableBody []
+        (case results of
+            Just results ->
+                (List.map searchResultsRow results)
+
+            Nothing ->
+                []
+        )
 
 
 searchResultsTable : Model -> Html Msg
 searchResultsTable model =
-    Html.text "Table"
+    table
+        { tableModifiers | fullWidth = True }
+        []
+        [ tableHead
+            []
+            [ tableRow
+                False
+                []
+                [ tableCellHead [] [ Html.text "ID" ]
+                , tableCellHead [] [ Html.text "Name" ]
+                ]
+            ]
+        , searchResultsBody model.results
+        ]
